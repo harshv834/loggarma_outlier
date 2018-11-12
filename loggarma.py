0601a64953781e3a0bac75df4201c8b266163182
harshv834 <harshv834@gmail.com>
2018-11-13 00:08:06 +0530

    dbeta equations updated

--- loggarma.py
+++ loggarma.py
@@ -6,7 +6,7 @@ c = 2
 def loggarma(X,Y,p,q,max_iter,t_0):
     Y_1 = np.max(Y,[c]*Y.shape[0])
     beta = [0]*(X.shape[1] - 1)
-    beta.insert(0,np.log(Y.sum()))
+    beta.insert(0,np.log(Y.sum()/Y.shape[0]))
     beta = np.array(beta)
     phi = np.random.rand(p)
     theta = np.random.rand(q)
@@ -25,14 +25,21 @@ def loggarma(X,Y,p,q,max_iter,t_0):
         dold_alpha = deta_alpha
 
         ##Check for convergence
-
-##Update gradients
-        deta_beta = X[:,0:beta.shape[0]]
-        for j in range(X.shape[0]):
-            if range
-            deta_beta[j:,j] =deta_beta[j:,j] - np.dot(beta,deta[])
-    
         
+##Update gradients
+        deta_beta = X[:,0:beta.shape[0]]            
+        np.inner(np.flip(phi1,axis=0).transpose(),a.transpose())
+        for j in range(X.shape[0] - phi.shape[0]):
+            X_block = X[j:j + phi.shape[0],:].transpose()
+            deta_beta -= np.inner(np.flip(phi,axis=0).transpose(),X_block).transpose()
+        for j in range(X.shape[0] - theta.shape[0]):
+            dbeta_block = dold_beta[j:j+theta.shape[0],:].transpose()
+            deta_beta -= np.inner(np.flip(theta,axis=0).transpose(),dbeta_block).transpose()
+            
+
+
+   
+#OLS minimization 
         mu = np.exp(eta)
         R = np.dot(deta_beta,beta) + np.dot(deta_phi,phi)+ np.dot(deta_theta,theta) + np.dot(deta_alpha,alpha)  + h*(Y - mu)*mu
         X_R = np.concatenate((deta_beta,deta_phi,deta_theta,deta_alpha),axis = 1)
